55f83886fe5667c6538c4fa11d74560d88ff0a89
Dhiego Henrique Balthazar de Sousa <dhiegobalthazar@gmail.com>
2023-12-28 23:18:11 -0300

    feat: add som e pontuação decrescente

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,21 +6,37 @@ const state = {
         score: document.querySelector("#score")
     },
     values: {
-        timerId: null,
         gameVelocity: 1000,
         hitPosition: null,
-        result: 0
+        result: 0,
+        currentTime: 60,
+    },
+    sounds: {
+        success: "./src/audios/success.mp3",
+        failed: "./src/audios/fail.mp3",
+    },
+    actions: {
+        timerId: setInterval(randomEnemy, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
     }
 };
+
+function countDown(){
+    state.values.currentTime--;
+    state.views.timeLeft.textContent = state.values.currentTime;
+    if(state.values.currentTime <= 0){
+        alert(`Acabou o Tempo!
+        Sua pontuação foi ${state.values.result}
+        `)
+        clearInterval(state.actions.timerId);
+        clearInterval(state.actions.countDownTimerId);
+    }
+}
 
 function clearEnemy(){
     state.views.squares.forEach((square) => {
         square.classList.remove("enemy");
     });
-}
-
-function moveEnemy(){
-    state.values.timerId = setInterval(randomEnemy, state.values.gameVelocity);
 }
 
 function randomEnemy(){
@@ -38,13 +54,32 @@ function addListenerHitBox(){
                 state.values.hitPosition = null;
                 state.values.result++;
                 state.views.score.textContent = state.values.result;
+                playSuccess();
+            } else {
+                if(state.values.hitPosition !== null){
+                    if(state.values.result > 0){
+                        state.values.result--;
+                    }
+                    state.views.score.textContent = state.values.result;
+                    playFailed();
+                }
             }
         })
     });
 }
 
+function playSuccess(){
+    const audio = new Audio(state.sounds.success);
+    audio.play();
+}
+
+function playFailed(){
+    const audio = new Audio(state.sounds.failed);
+    audio.play();
+}
+
+
 function init(){
-    moveEnemy();
     addListenerHitBox();
 }
 
